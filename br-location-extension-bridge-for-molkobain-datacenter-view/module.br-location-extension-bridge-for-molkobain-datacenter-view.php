e5fe8c0038c49ae5d16554f4e8383087ce23477d
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-08-02 12:13:23 +0200

    add compatibility to datacenter-view

--- br-location-extension-bridge-for-molkobain-datacenter-view/module.br-location-extension-bridge-for-molkobain-datacenter-view.php
+++ br-location-extension-bridge-for-molkobain-datacenter-view/module.br-location-extension-bridge-for-molkobain-datacenter-view.php
@@ -1,6 +1,7 @@
 <?php
 
 /**
+ * @copyright   Copyright (C) 2024 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
  * @version     2024-08-02
  *
@@ -9,37 +10,35 @@
 
 SetupWebPage::AddModule(
     __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-    'br-location-extension/0.6.0',
+    'br-location-extension-bridge-for-datacenterview/0.0.1',
     array(
         // Identification
-        //
-        'label' => 'Location enhancements',
+        'label' => 'Bridge - Location Extension - Molkobain Datacenter View',
         'category' => 'business',
 
         // Setup
-        //
         'dependencies' => array(
-            '(itop-config-mgmt/2.5.0 & itop-config-mgmt/<3.0.0)||itop-structure/3.0.0',
+            'molkobain-datacenter-view/1.6.0',
+            'br-location-extension/0.6.0',
         ),
         'mandatory' => false,
         'visible' => true,
+        'auto_select' => 'SetupInfo::ModuleIsSelected("molkobain-datacenter-view") && SetupInfo::ModuleIsSelected("br-location-extension")',
 
         // Components
-        //
         'datamodel' => array(
-            'model.br-location-extension.php',
+            'model.br-location-extension-bridge-for-molkobain-datacenter-view.php'
         ),
         'webservice' => array(),
+        'dictionary' => array(),
         'data.struct' => array(),
         'data.sample' => array(),
 
         // Documentation
-        //
         'doc.manual_setup' => '',
         'doc.more_information' => '',
 
         // Default settings
-        //
         'settings' => array(),
     )
 );
